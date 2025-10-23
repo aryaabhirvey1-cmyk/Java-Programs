da0a1cf477552a2999af6c638b700756b6fa3a88
ARYA_ABHIRVEY <aryaabhirvey1@gmail.com>
2025-10-23 22:37:04 +0530

    ADDED

---
+++ Sumofn.java
@@ -0,0 +1,9 @@
+public class Sumofn{
+    public static void main(String[] args){
+        int sum = 0;
+        int n = 50;
+        for(int i = 0;i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
